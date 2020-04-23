c049b65180bb95add5319cff40e3ad33efb614b0
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-04-23 13:49:44 +0900

    use angle

--- src/containers/index.js
+++ src/containers/index.js
@@ -79,6 +79,7 @@ class App extends Container {
             layers={[
               new MovesLayer({
                 routePaths, getRouteWidth:()=>2,
+                getOrientation: (x) => x.angle ? [0,(x.angle * -1),90] : [0,0,90],
                 movesbase, movedData,
                 iconDesignations:[
                   {type:'vehicle', layer:'SimpleMesh', getColor:vehicleColor, sizeScale:1, getScale:vehicleScale},
