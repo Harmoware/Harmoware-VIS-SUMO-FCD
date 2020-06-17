fbdfa797132d59f5d5f8bc83bc281e997d3cdb0c
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-06-17 09:32:10 +0900

    version up

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
     }, {
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"]
+    }, {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
     }]
   },
   plugins: [
